a0dbf4335eb99e0bb2a3bc5943760c882d333d76
Artur Semenas <uncelfuka@gmail.com>
2024-02-19 23:48:40 +0100

    Module #3. Task #1. SQL (correct)

--- lesson4/src/main/resources/SOLUTION.sql
+++ lesson4/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
-select * from students;
-select * from students LIMIT 50;
+select * from student;
+select * from student LIMIT 50;
 select name from student;
 SELECT DISTINCT amount FROM payment;
--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -1,5 +1,5 @@
-select * from payments where amount>=500;
-select * from students where dateadd('YEAR', 20, current_date) < birthday;
+select * from payment where amount>=500;
+select * from student where dateadd('YEAR', 20, current_date) < birthday;
 SELECT * FROM Student WHERE groupnumber = 10 AND dateadd('YEAR', 20, current_date) > birthday;
 SELECT * FROM Student WHERE name = 'Mike' OR groupnumber IN (4, 5, 6);
 SELECT * FROM Payment WHERE payment_date < dateadd('MONTH', -8, current_date);
