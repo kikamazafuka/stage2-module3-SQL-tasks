ecebc0320d9e3ca951b0a7ec805bf5ad97b20e04
Artur Semenas <uncelfuka@gmail.com>
2024-02-20 17:09:11 +0100

    Module #3. Task #1. SQL (Tables_changes, les 5, les 3)

--- lesson12/src/main/resources/TABLES_CHANGES.sql
+++ lesson12/src/main/resources/TABLES_CHANGES.sql
@@ -1,10 +1,9 @@
-select * from student;
-ALTER TABLE mark DROP FOREIGN KEY IF EXISTS mark_student_id_fkey;
+ALTER TABLE mark DROP CONSTRAINT IF EXISTS mark_student_id_fkey;
 ALTER TABLE mark ADD CONSTRAINT mark_student_id_fkey FOREIGN KEY (student_id) REFERENCES student(id) ON DELETE CASCADE;
-ALTER TABLE mark DROP FOREIGN KEY IF EXISTS mark_subject_id_fkey;
+ALTER TABLE mark DROP CONSTRAINT IF EXISTS mark_subject_id_fkey;
 ALTER TABLE mark ADD CONSTRAINT mark_subject_id_fkey FOREIGN KEY (subject_id) REFERENCES subject(id) ON DELETE CASCADE;
-ALTER TABLE payment DROP FOREIGN KEY IF EXISTS payment_type_id_fkey;
+ALTER TABLE mark DROP CONSTRAINT IF EXISTS payment_type_id_fkey;
 ALTER TABLE payment ADD CONSTRAINT payment_type_id_fkey FOREIGN KEY (type_id) REFERENCES paymenttype(id) ON DELETE CASCADE;
-ALTER TABLE payment DROP FOREIGN KEY IF EXISTS payment_student_id_fkey;
+ALTER TABLE mark DROP CONSTRAINT IF EXISTS payment_student_id_fkey;
 ALTER TABLE payment ADD CONSTRAINT payment_student_id_fkey FOREIGN KEY (student_id) REFERENCES student(id) ON DELETE CASCADE;
 
--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -1,7 +1,7 @@
 select * from payment where amount>=500;
-select * from student where dateadd('YEAR', 20, current_date) < birthday;
-SELECT * FROM Student WHERE groupnumber = 10 AND dateadd('YEAR', 20, current_date) > birthday;
+select * from student where dateadd('YEAR', -20, current_date) > birthday;
+SELECT * FROM Student WHERE groupnumber = 10 AND dateadd('YEAR', -20, current_date) < birthday;
 SELECT * FROM Student WHERE name = 'Mike' OR groupnumber IN (4, 5, 6);
-SELECT * FROM Payment WHERE payment_date < dateadd('MONTH', -8, current_date);
+SELECT * FROM Payment WHERE payment_date >= dateadd('MONTH', -8, current_date);
 SELECT * FROM Student WHERE name LIKE 'A%';
 SELECT * FROM Student WHERE (name = 'Roxi' AND groupnumber = 4) OR (name = 'Tallie' AND groupnumber = 9);
